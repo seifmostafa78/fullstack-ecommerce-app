10582d716bb58244ebe6686a3a2de18a463e0a20
seifmostafa78 <seifmostafaali78@gmail.com>
2025-06-16 17:59:48 +0300

    edit allowedOrigins.js

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fullstack-ecommerce-app-iota.vercel.app",
+];
 
 module.exports = allowedOrigins;
